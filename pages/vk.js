868c855511845b6edc7d388c264edc8f5eb6e63b
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-03 18:14:23 +0300

    Add files via upload

---
+++ pages/vk.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import Image from 'next/image'
+
+import vk from './vk.png'
+
+import inst from './inst.jpg'
+
+import {NavLink} from "react-router-dom"
+
+import s from './form.module.css'
+
+
+
+
+
+const VK = () => {
+
+
+
+return <div className = {s.vk_wrapper}>
+
+<div className =  {s.vk_grid} >
+
+<Image src = '' width = "100%" height= ""  layout = "intrinsic" />
+
+<img src = "http://sun9-39.userapi.com/impg/7X5xDkptQdcQ04lyi212DCQL74a_gNiWMYsc3Q/TSEGVX2IMdc.jpg?size=1024x1024&quality=96&sign=378e0012ef4c2db7d7f5a6d214e2cb4e&type=album"/>
+
+</div>
+
+
+
+</div>
+
+
+
+}
+
+
+
+
+
+export default VK;
+
